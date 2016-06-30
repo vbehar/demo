fe103c44f34cf57e27cc91fe85ea9abf6d88968b
Vincent Behar <v.behar@free.fr>
2016-06-30 17:41:26 +0200

    ui

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var content = "\
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' integrity='sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7' crossorigin='anonymous'>\
 </head>\
 <body>\
-<div style='text-align: center; padding: 40px 15px;'>\
+<div style='text-align: center; padding: 40px 15px; background: blue;'>\
 <h1>Hello !</h1>\
 <h2>I'm alive !</h2>\
 </div>\
